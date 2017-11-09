038664033ccdce1d3894a9c2705cd22fbddf6ce5
Johnnyhaha <31158411+Johnnyhaha@users.noreply.github.com>
2017-11-09 21:02:06 +0800

    Creat UI and initial viewcontroller

---
+++ Mosaic/ViewControllers/ChoosePhotoViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ChoosePhotoViewController.swift
+//  Mosaic
+//
+//  Created by Johnny_L on 2017/11/9.
+//  Copyright © 2017年 Johnny_L. All rights reserved.
+//
+
+import UIKit
+import Metal
+
+class ChoosePhotoViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+
+    // The Metal device we use to perform Metal operations
+    var device: MTLDevice!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        device = MTLCreateSystemDefaultDevice()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    
+
+}
